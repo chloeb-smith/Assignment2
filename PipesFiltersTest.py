162de910e2a2662f461dc5352810ac717558bc01
Chloe Smith <158786659+chloeb-smith@users.noreply.github.com>
2025-04-14 13:27:21 -0400

    modified formatting

--- PipesFiltersTest.py
+++ PipesFiltersTest.py
@@ -5,12 +5,10 @@ import KWICPipesFilters as pipes
 
 def main():
     filters = [pipes.KWIC_input, pipes.alphabetize, pipes.circular_shifter]
-
     string_data = 'apples bananas oranges'
+    list_data = ['apple', 'banana', 'orange']
 
     assert pipes.pipeline(string_data, filters) == ['oranges', 'bananas', 'apples']
-
-    list_data = ['apple', 'banana', 'orange']
     assert pipes.pipeline(list_data, filters) == ['orange', 'banana', 'apple']
 
     print('All tests passed.')
